ffd578728798e6a3561aebf28ad6ce62cea0c496
Chris Mesidor <christopher.mesidor1996@gmail.com>
2021-09-03 14:33:35 -0400

    Fixed  connection string but now NullReferenceException: Object reference not set to an instance of an object. in Views.Home.Index

--- P1_ASP_WebApp/Startup.cs
+++ P1_ASP_WebApp/Startup.cs
@@ -1,13 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RR_BL;
+using RR_DL;
+using RR_DL.Entities;
 
 namespace P1_ASP_WebApp
 {
@@ -21,8 +22,25 @@ namespace P1_ASP_WebApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            if (Configuration["OtherRepository"] == "true")
+            {
+                //services.AddScoped<IRepository, NonEfRepository>();
+            }
+            else
+            {
+                // "if a class asks for an IRepository, give it a Repository"
+                services.AddScoped<IRepo, Repo>();
+            }
+            services.AddDbContext<CMDBP0Context>(options =>
+            {
+                options.UseSqlServer(File.ReadAllText(@"C:\Users\cm121\cmdbp0-connect.txt"));
+                options.LogTo(Console.WriteLine);
+            });
+
             services.AddControllersWithViews();
         }
 
